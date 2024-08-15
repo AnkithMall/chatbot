451a96cdd4603382e71594fde8cec07c4017f51a
AnkithMall <ankithmall1729@gmail.com>
2024-08-15 21:30:07 +0530

    prototype v1 - added redirect for deployment

--- frontend/chatbot/src/App.jsx
+++ frontend/chatbot/src/App.jsx
@@ -4,10 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return <>
-
-
     <BrowserRouter>
-
       <Routes>
         <Route path="/show_chats" element={<ChatMessages />} />
         <Route path="/" element={<ChatBot />} />
